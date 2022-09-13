9ebc5d02f4de4ee9808f4c2c25a7d39238d6d125
fonser12 <fonser12@RTCMW031930>
2022-09-13 18:01:10 +0200

    initial

---
+++ src/net6core/netcore/Startup.cs
@@ -0,0 +1,56 @@
+﻿namespace netcore
+{
+    public class Startup
+    {
+
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+
+        public void ConfigurationService(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+
+            app.MapFallbackToFile("index.html");
+
+          
+        }
+ 
+
+    }
+}
